d76c9072e5cd3b66d64806b9c782f8b93a6fa196
t-yosfukuda <t-yosfukuda@yahoo-corp.jp>
2019-05-08 16:08:18 +0900

    edit

--- vars.sh
+++ vars.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-export JRE_URL=https://download.oracle.com/otn/java/jdk/8u212-b10/59066701cf1a433da9770636fbc4c9aa/jdk-8u212-linux-x64.tar.gz
+export JRE_URL=${JRE_LATEST_URL}
 export RUNDECK_DIST_URL="http://dl.bintray.com/rundeck/rundeck-maven/"
 export RUNDECK_WAR="rundeck-${RUNDECK_VERSION}.war"
